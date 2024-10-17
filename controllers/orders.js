e1755ea8cb9b2c7b2aef817f25e811df0fb08816
samee185 <samueliy185@gmail.com>
2024-10-17 18:06:29 +0100

    updates

--- controllers/orders.js
+++ controllers/orders.js
@@ -105,17 +105,13 @@ const updateOrderStatus = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc Get all orders for a user
-// @route GET /api/orders/myorders
-// @access Private
+
 const getMyOrders = asyncHandler(async (req, res) => {
   const orders = await Order.find({ user: req.user._id });
   res.json(orders);
 });
 
-// @desc Get all orders (Admin)
-// @route GET /api/orders
-// @access Private/Admin
+
 const getOrders = asyncHandler(async (req, res) => {
   const orders = await Order.find({}).populate("user", "id name");
   res.json(orders);
